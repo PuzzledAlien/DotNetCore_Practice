7eb88485c9b036d3d1d0bf962783cbdfc9d5d06f
alien <734329218@qq.com>
2019-02-18 17:08:19 +0800

    完善注释

--- 如何在控制台应用(.Net Core)使用appsetting.json配置/ConsoleApp1/Program.cs
+++ 如何在控制台应用(.Net Core)使用appsetting.json配置/ConsoleApp1/Program.cs
@@ -16,15 +16,20 @@ namespace ConsoleApp1
                 _appConfiguration = AppConfigurations.Get(hostingEnvironment.ContentRootPath, hostingEnvironment.EnvironmentName);
             }).ConfigureServices((hostContext, services) =>
             {
+                //注入IConfiguration到DI容器
                 services.AddSingleton(_appConfiguration);
 
+                //注入MyService到DI容器
                 services.AddSingleton<IMyService, MyService>();
             });
 
+            //初始化通用主机
             var host = hostBuilder.Build();
 
+            //获取MyService
             var myService = host.Services.GetService<IMyService>();
 
+            //调用SayMyWords方法
             myService.SayMyWords();
 
             Console.ReadKey();
