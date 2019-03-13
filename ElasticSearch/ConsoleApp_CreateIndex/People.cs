3f29118b402ad862609fc3dc7956b5e27fe89dee
alien <734329218@qq.com>
2019-03-13 16:50:31 +0800

    ElasticSearch .Net Client NEST Demo

---
+++ ElasticSearch/ConsoleApp_CreateIndex/People.cs
@@ -0,0 +1,24 @@
+﻿//=====================================================
+
+//Copyright (C) 2016-2019 Fanjia
+
+//All rights reserved
+
+//CLR版 本:    4.0.30319.42000
+
+//创建时间:     2019/3/13 16:36:34
+
+//创 建 人:   徐晓航
+
+//======================================================
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_CreateIndex
+{
+    public class People
+    {
+    }
+}
---
+++ ElasticSearch/ConsoleApp_CreateIndex/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Elasticsearch.Net;
+using Nest;
+
+namespace ConsoleApp_CreateIndex
+{
+    class Program
+    {
+        private static ElasticClient _elasticClient;
+        private static string _indexName;
+        static void Main(string[] args)
+        {
+            _indexName = "TEST";
+            _indexName = _indexName.ToLower();//索引名称一定要小写
+            _elasticClient = GetElasticClient();
+            
+            var existsResponse = _elasticClient.IndexExists(_indexName);
+            if (!existsResponse.Exists)
+            {
+                //基本配置
+                IIndexState indexState = new IndexState
+                {
+                    Settings = new IndexSettings
+                    {
+                        NumberOfReplicas = 1,//副本数
+                        NumberOfShards = 6//分片数
+                    }
+                };
+
+                ICreateIndexResponse response = _elasticClient.CreateIndex(_indexName, p => p
+                    .InitializeUsing(indexState)
+                    .Mappings(m => m.Map<People>(r => r.AutoMap()))
+                );
+
+                if (response.IsValid)
+                {
+                    Console.WriteLine("索引创建成功");
+                }
+                else
+                {
+                    Console.WriteLine("索引创建失败");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        public static ElasticClient GetElasticClient()
+        {
+            var client = new ElasticClient();
+            return client;
+        }
+
+        public static ElasticClient GetElasticClient(string uri)
+        {
+            var settings = new ConnectionSettings(new Uri(uri)).
+            DefaultIndex(_indexName);
+            settings.EnableDebugMode();
+            var client = new ElasticClient(settings);
+            return client;
+        }
+
+        public static ElasticClient GetElasticClient(Uri uri)
+        {
+            var client = new ElasticClient(uri);
+            return client;
+        }
+
+        public static ElasticClient GetElasticClientByPool()
+        {
+            var uris = new[]
+            {
+                new Uri("http://localhost:9200")
+            };
+
+            var connectionPool = new SniffingConnectionPool(uris);
+
+            var settings = new ConnectionSettings(connectionPool)
+                .DefaultIndex(_indexName);
+
+            settings.EnableDebugMode();//调试debug使用该模式，正式环境请去掉
+
+            var client = new ElasticClient(settings);
+
+            return client;
+        }
+    }
+}
