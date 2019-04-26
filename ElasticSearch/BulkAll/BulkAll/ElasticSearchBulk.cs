3d68ccc8d22bd292a3f6adbd8ca2a04892c2cf14
alien <734329218@qq.com>
2019-04-26 17:37:12 +0800

    Add BulkAll

---
+++ ElasticSearch/BulkAll/BulkAll/ElasticSearchBulk.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Nest;
+using static System.Console;
+
+namespace BulkAll
+{
+    public class ElasticSearchBulk
+    {
+        public static bool CreateIndex<T>(IElasticClient elasticClient, string indexName) where T : class
+        {
+            var existsResponse = elasticClient.IndexExists(indexName);
+            // 存在则返回true 不存在创建
+            if (existsResponse.Exists)
+            {
+                return true;
+            }
+            //基本配置
+            IIndexState indexState = new IndexState
+            {
+                Settings = new IndexSettings
+                {
+                    NumberOfReplicas = 1,//副本数
+                    NumberOfShards = 6//分片数
+                }
+            };
+
+            ICreateIndexResponse response = elasticClient.CreateIndex(indexName, p => p
+                .InitializeUsing(indexState).Mappings(m => m.Map<T>(r => r.AutoMap()))
+            );
+
+            return response.IsValid;
+        }
+        public static ElasticClient GetElasticClient()
+        {
+            var client = new ElasticClient();
+            return client;
+        }
+        public static bool BulkAll<T>(IElasticClient elasticClient, IndexName indexName, IEnumerable<T> list) where T : class
+        {
+            const int size = 1000;
+            var tokenSource = new CancellationTokenSource();
+
+            var observableBulk = elasticClient.BulkAll(list, f => f
+                    .MaxDegreeOfParallelism(8)
+                    .BackOffTime(TimeSpan.FromSeconds(10))
+                    .BackOffRetries(2)
+                    .Size(size)
+                    .RefreshOnCompleted()
+                    .Index(indexName)
+                    .BufferToBulk((r, buffer) => r.IndexMany(buffer))
+                , tokenSource.Token);
+
+            var countdownEvent = new CountdownEvent(1);
+
+            Exception exception = null;
+
+            var bulkAllObserver = new BulkAllObserver(
+                onNext: response =>
+                {
+                    WriteLine($"Indexed {response.Page * size} with {response.Retries} retries");
+                },
+                onError: ex =>
+                {
+                    WriteLine("BulkAll Error : {0}", ex);
+                    exception = ex;
+                    countdownEvent.Signal();
+                },
+                onCompleted: () =>
+                {
+                    WriteLine("BulkAll Finished");
+                    countdownEvent.Signal();
+                });
+
+            observableBulk.Subscribe(bulkAllObserver);
+
+            countdownEvent.Wait(tokenSource.Token);
+
+            if (exception != null)
+            {
+                WriteLine("BulkHotelGeo Error : {0}", exception);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ ElasticSearch/BulkAll/BulkAll/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace BulkAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int numberOfCycles = 20000;
+
+            var sw = Stopwatch.StartNew();
+
+            var client = ElasticSearchBulk.GetElasticClient();
+            var indexName = nameof(TestNum).ToLower();//索引名称小写
+            var createSuccess = ElasticSearchBulk.CreateIndex<TestNum>(client, indexName);
+            if (createSuccess)
+            {
+                var list = new List<TestNum>(numberOfCycles);
+                for (var i = 0; i < numberOfCycles; i++)
+                {
+                    var item = new TestNum { I = i, Msg = $"testNum: {i} " };
+                    list.Add(item);
+                }
+
+                ElasticSearchBulk.BulkAll(client, indexName, list);
+            }
+            
+
+            sw.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine("Ellapsed: {0}, numPerSec: {1}", sw.ElapsedMilliseconds, numberOfCycles / (sw.ElapsedMilliseconds / (double)1000));
+            Console.ReadKey();
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class TestNum
+    {
+        public int I { get; set; }
+        public string Msg { get; set; }
+    }
+}
