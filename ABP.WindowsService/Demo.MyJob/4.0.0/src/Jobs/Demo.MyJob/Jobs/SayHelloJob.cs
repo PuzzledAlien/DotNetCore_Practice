c3d0c5d8791bc87f1f7872a3e0e087d1212122ca
alien <734329218@qq.com>
2019-06-13 16:26:36 +0800

    Create SayHelloJob.cs

---
+++ ABP.WindowsService/Demo.MyJob/4.0.0/src/Jobs/Demo.MyJob/Jobs/SayHelloJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace Demo.MyJob.Jobs
+{
+    class SayHelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
