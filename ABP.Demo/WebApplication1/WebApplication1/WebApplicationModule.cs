a74604915df1236253d542fc68966fc918728c33
alien <734329218@qq.com>
2019-07-17 16:50:12 +0800

    Create WebApplicationModule.cs

---
+++ ABP.Demo/WebApplication1/WebApplication1/WebApplicationModule.cs
@@ -0,0 +1,15 @@
+﻿using Abp.AspNetCore;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+
+namespace WebApplication1
+{
+    [DependsOn(typeof(AbpAspNetCoreModule))]
+    public class WebApplicationModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(WebApplicationModule).GetAssembly());
+        }
+    }
+}
