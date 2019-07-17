55cca53066437c9b7eb674324eaae1cf95be1530
alien <734329218@qq.com>
2019-07-17 17:34:11 +0800

    Update WebApplicationModule.cs
    配置DefaultWrapResultAttribute

--- ABP.Demo/WebApplication1/WebApplication1/WebApplicationModule.cs
+++ ABP.Demo/WebApplication1/WebApplication1/WebApplicationModule.cs
@@ -1,12 +1,21 @@
 ﻿using Abp.AspNetCore;
+using Abp.AspNetCore.Configuration;
 using Abp.Modules;
 using Abp.Reflection.Extensions;
+using Abp.Web.Models;
 
 namespace WebApplication1
 {
     [DependsOn(typeof(AbpAspNetCoreModule))]
     public class WebApplicationModule : AbpModule
     {
+        public override void PreInitialize()
+        {
+            var result = new DontWrapResultAttribute();
+            Configuration.Modules.AbpAspNetCore().DefaultWrapResultAttribute.WrapOnError = result.WrapOnError;
+            Configuration.Modules.AbpAspNetCore().DefaultWrapResultAttribute.WrapOnSuccess = result.WrapOnSuccess;
+        }
+
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(typeof(WebApplicationModule).GetAssembly());
