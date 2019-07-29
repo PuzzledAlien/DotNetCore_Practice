761e5bdf4bd75a509ddd6c38b22a7a29599dcd7f
alien <734329218@qq.com>
2019-07-29 16:39:43 +0800

    Add JobCodeModule
    将Job初始化逻辑抽出来

---
+++ ABP.WindowsService/Demo.MyJob/4.0.0/src/Jobs/Demo.MyJob/JobCoreModule.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Reflection;
+using Abp.Modules;
+using Castle.Windsor.MsDependencyInjection;
+using Demo.MyJob.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Topshelf;
+
+namespace Demo.MyJob
+{
+    [DependsOn(typeof(MyJobApplicationModule))]
+    public class JobCoreModule : AbpModule
+    {
+        /// <summary>
+        /// 日志配置文件路径
+        /// </summary>
+        public string LogConfigFile { get; set; }
+
+        /// <summary>
+        /// Windows服务的描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Windows服务的显示名称
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Windows服务的服务名称
+        /// </summary>
+        public string ServiceName { get; set; }
+
+        public IConfiguration AppConfiguration { get; set; }
+
+        public override void PreInitialize()
+        {
+            var host = new HostBuilder().ConfigureAppConfiguration((hostContext, configApp) =>
+            {
+                var hostingEnvironment = hostContext.HostingEnvironment;
+                AppConfiguration = AppConfigurations.Get(hostingEnvironment.ContentRootPath, hostingEnvironment.EnvironmentName);
+            }).ConfigureServices((hostContext, services) =>
+            {
+                services.AddSingleton(AppConfiguration);
+
+                WindsorRegistrationHelper.CreateServiceProvider(IocManager.IocContainer, services);
+            });
+
+            host.Build();
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+
+        public override void PostInitialize()
+        {
+            log4net.GlobalContext.Properties["LogsDirectory"] = AppDomain.CurrentDomain.BaseDirectory;
+
+            HostFactory.Run(configure =>
+            {
+                //定义服务描述
+                configure.SetDescription(Description);
+                configure.SetDisplayName(DisplayName);
+                configure.SetServiceName(ServiceName);
+
+                configure.RunAsLocalSystem();
+
+                //使用log4net记录日志
+                configure.UseLog4Net("App.config");
+
+                //定义操作
+                configure.Service<MyJobService>(service =>
+                {
+                    service.ConstructUsing(_ => new MyJobService());
+                    service.WhenStarted(async _ => await _.StartAsync());
+                    service.WhenStopped(async _ => await _.StopAsync());
+                    service.WhenContinued(async _ => await _.ContinueAsync());
+                    service.WhenPaused(async _ => await _.PauseAsync());
+                });
+            });
+        }
+    }
+}
--- ABP.WindowsService/Demo.MyJob/4.0.0/src/Jobs/Demo.MyJob/MyJobAbpModule.cs
+++ ABP.WindowsService/Demo.MyJob/4.0.0/src/Jobs/Demo.MyJob/MyJobAbpModule.cs
@@ -1,67 +1,12 @@
-﻿using System;
-using System.Reflection;
-using Abp.Modules;
-using Castle.Windsor.MsDependencyInjection;
-using Demo.MyJob.Configuration;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Topshelf;
-
-namespace Demo.MyJob
+﻿namespace Demo.MyJob
 {
-    [DependsOn(typeof(MyJobApplicationModule))]
-    public class MyJobAbpModule : AbpModule
+    public class MyJobAbpModule : JobCoreModule
     {
-        public IConfiguration AppConfiguration { get; set; }
-
-        public override void PreInitialize()
+        public MyJobAbpModule()
         {
-            var host = new HostBuilder().ConfigureAppConfiguration((hostContext, configApp) =>
-            {
-                var hostingEnvironment = hostContext.HostingEnvironment;
-                AppConfiguration = AppConfigurations.Get(hostingEnvironment.ContentRootPath, hostingEnvironment.EnvironmentName);
-            }).ConfigureServices((hostContext, services) =>
-            {
-                services.AddSingleton(AppConfiguration);
-
-                WindsorRegistrationHelper.CreateServiceProvider(IocManager.IocContainer, services);
-            });
-
-            host.Build();
-        }
-
-        public override void Initialize()
-        {
-            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
-        }
-
-        public override void PostInitialize()
-        {
-            log4net.GlobalContext.Properties["LogsDirectory"] = AppDomain.CurrentDomain.BaseDirectory;
-
-            HostFactory.Run(configure =>
-            {
-                //定义服务描述
-                configure.SetDescription("Demo.MyJob Service");
-                configure.SetDisplayName("Demo.MyJob");
-                configure.SetServiceName("Demo.MyJob");
-
-                configure.RunAsLocalSystem();
-
-                //使用log4net记录日志
-                configure.UseLog4Net("App.config");
-
-                //定义操作
-                configure.Service<MyJobService>(service =>
-                {
-                    service.ConstructUsing(_ => new MyJobService());
-                    service.WhenStarted(async _ => await _.StartAsync());
-                    service.WhenStopped(async _ => await _.StopAsync());
-                    service.WhenContinued(async _ => await _.ContinueAsync());
-                    service.WhenPaused(async _ => await _.PauseAsync());
-                });
-            });
+            Description = "Demo.MyJob Service";
+            DisplayName = "Demo.MyJob";
+            ServiceName = DisplayName;
         }
     }
 }
