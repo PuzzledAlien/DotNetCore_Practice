919bf0a4151ba88350d1034791a6bdff765317b7
AllenXu <734329218@qq.com>
2020-10-22 19:46:01 +0800

    Update AppConfigurations.cs

--- 如何在控制台应用(.Net Core)使用appsetting.json配置/ConsoleApp1/AppConfigurations.cs
+++ 如何在控制台应用(.Net Core)使用appsetting.json配置/ConsoleApp1/AppConfigurations.cs
@@ -1,18 +1,4 @@
-﻿//=====================================================
-
-//Copyright (C) 2016-2019 Fanjia
-
-//All rights reserved
-
-//CLR版 本:    4.0.30319.42000
-
-//创建时间:     2019/2/18 16:38:41
-
-//创 建 人:   徐晓航
-
-//======================================================
-
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using Microsoft.Extensions.Configuration;
 
 namespace ConsoleApp1
