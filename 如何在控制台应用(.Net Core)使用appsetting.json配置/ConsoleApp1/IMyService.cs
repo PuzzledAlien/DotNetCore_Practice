4f801cc60fbe6c0fb8bf2d4370a052e804c80d52
AllenXu <734329218@qq.com>
2020-10-22 19:45:31 +0800

    Update IMyService.cs

--- 如何在控制台应用(.Net Core)使用appsetting.json配置/ConsoleApp1/IMyService.cs
+++ 如何在控制台应用(.Net Core)使用appsetting.json配置/ConsoleApp1/IMyService.cs
@@ -1,22 +1,4 @@
-﻿//=====================================================
-
-//Copyright (C) 2016-2019 Fanjia
-
-//All rights reserved
-
-//CLR版 本:    4.0.30319.42000
-
-//创建时间:     2019/2/18 16:46:52
-
-//创 建 人:   徐晓航
-
-//======================================================
-
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     public interface IMyService
     {
